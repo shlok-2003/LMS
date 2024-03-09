5bdb4328d83aa61b321d8553b1bc09bf2489b166
shlok-2003 <shlokprajapati2003@gmail.com>
2024-03-09 14:48:10 +0530

    Added cors

--- server/index.js
+++ server/index.js
@@ -23,7 +23,12 @@ database.connect();
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors(
+	{
+		origin: "https://studynotion-frontend-weld.vercel.app",
+		credentials:true,
+	}
+));
 app.use(morgan("dev"));	
 
 app.use(
